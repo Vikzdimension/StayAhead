03308b986a7dbb5e6c42e16279e66bef890fce4f
Vivek Lode <vivek.lode1@gmail.com>
2025-02-13 18:19:25 +0530

    Task Schedular Created
    Task Schedular Created

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Console\Schedules\TaskScheduler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(TaskScheduler::class);
     }
 
     /**
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->make(TaskScheduler::class)->register(app('Illuminate\Console\Scheduling\Schedule'));
     }
 }
